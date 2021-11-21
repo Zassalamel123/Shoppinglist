ed4d8908963955710915c3f163c04d6c444ad6b2
Marco Bräumer <marco.braeumer@students.hs-mainz.de>
2021-11-21 07:56:37 +0100

    add new colors, feature to remove item from list

--- app/src/main/java/com/example/einkaufslisteapp/ListCreationActivity.java
+++ app/src/main/java/com/example/einkaufslisteapp/ListCreationActivity.java
@@ -29,6 +29,7 @@ public class ListCreationActivity extends AppCompatActivity {
         goToPreviousActivity();
         getTitleListFromPreviousActivity();
         addItem();
+        deleteLastItem();
         saveShoppingList();
     }
 
@@ -69,15 +70,11 @@ public class ListCreationActivity extends AppCompatActivity {
 
     private void addItem() {
         ImageView addItem = findViewById(R.id.toolBarAdd);
-        addItem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText item = generateItemField();
-                applyItemToView(item);
-                saveEditTextItemToList(item);
-            }
+        addItem.setOnClickListener(v -> {
+            EditText item = generateItemField();
+            applyItemToView(item);
+            saveEditTextItemToList(item);
         });
-
     }
 
     private EditText generateItemField() {
@@ -99,8 +96,8 @@ public class ListCreationActivity extends AppCompatActivity {
             constraintSet.connect(item.getId(), ConstraintSet.TOP, R.id.listTitle, ConstraintSet.BOTTOM, 16);
             constraintSet.connect(item.getId(), ConstraintSet.LEFT, R.id.constraintLayout, ConstraintSet.LEFT);
             constraintSet.connect(item.getId(), ConstraintSet.RIGHT, R.id.constraintLayout, ConstraintSet.RIGHT);
-        } else{
-            EditText lastItem = editTextItems.get(editTextItems.size()-1);
+        } else {
+            EditText lastItem = editTextItems.get(editTextItems.size() - 1);
             constraintSet.connect(item.getId(), ConstraintSet.TOP, lastItem.getId(), ConstraintSet.BOTTOM, 16);
             constraintSet.connect(item.getId(), ConstraintSet.LEFT, R.id.constraintLayout, ConstraintSet.LEFT);
             constraintSet.connect(item.getId(), ConstraintSet.RIGHT, R.id.constraintLayout, ConstraintSet.RIGHT);
@@ -113,9 +110,41 @@ public class ListCreationActivity extends AppCompatActivity {
         editTextItems.add(item);
     }
 
-    private void deleteItem() {
-
+    private void deleteLastItem() {
+        ImageView deleteItem = findViewById(R.id.toolBarDelete);
+        deleteItem.setOnClickListener(v -> {
+            if (isFieldEmpty()) {
+                toastFieldCanNotBeRemoved();
+            } else {
+                int lastEntry = editTextItems.size() - 1;
+                EditText lastItem = editTextItems.get(lastEntry);
+                deleteField(lastItem);
+                removeLastEditTextItemFromList(lastItem);
+            }
+        });
     }
+
+    private void deleteField(EditText lastItem) {
+        ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
+        constraintLayout.removeView(lastItem);
+    }
+
+    private boolean isFieldEmpty() {
+        if (editTextItems.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    private void toastFieldCanNotBeRemoved() {
+        Toast toastNoEmptyField = Toast.makeText(this, "Es sind keine Felder vorhanden, die gelöscht werden können", Toast.LENGTH_SHORT);
+        toastNoEmptyField.show();
+    }
+
+    private void removeLastEditTextItemFromList(EditText item) {
+        editTextItems.remove(item);
+    }
+
 
     private List<String> saveItemsToList() {
         List<String> items = new ArrayList<>();
@@ -132,20 +161,17 @@ public class ListCreationActivity extends AppCompatActivity {
         Toast toastEmptyField = Toast.makeText(this, "Es sind leere Felder vorhanden, bitte alle ausfüllen", Toast.LENGTH_SHORT);
         Toast toastDuplicateField = Toast.makeText(this, "Die Feldernamen müssen einmalig sein, bitte ändern", Toast.LENGTH_SHORT);
 
-        saveListImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (areFieldsEmpty() | isTitleEmpty()) {
-                    toastEmptyField.show();
-                } else if (areFieldNamesDuplicated()) {
-                    toastDuplicateField.show();
-                } else {
-                    String title = editText.getText().toString();
-                    managerList.saveItemList(title, saveItemsToList());
-                    Intent intent = new Intent(ListCreationActivity.this, ManagerListActivity.class);
-                    ListCreationActivity.super.finish();
-                    startActivity(intent);
-                }
+        saveListImage.setOnClickListener(v -> {
+            if (areFieldsEmpty() | isTitleEmpty()) {
+                toastEmptyField.show();
+            } else if (areFieldNamesDuplicated()) {
+                toastDuplicateField.show();
+            } else {
+                String title = editText.getText().toString();
+                managerList.saveItemList(title, saveItemsToList());
+                Intent intent = new Intent(ListCreationActivity.this, ManagerListActivity.class);
+                ListCreationActivity.super.finish();
+                startActivity(intent);
             }
         });
     }
