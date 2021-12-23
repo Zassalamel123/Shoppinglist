bce3bf5c7a2bcd76ea3e6a538455513052f4f2cd
Marco Bräumer <marco.braeumer@students.hs-mainz.de>
2021-12-23 11:25:34 +0100

    activity tests for list creation and delete

---
+++ app/src/androidTest/java/com/example/einkaufslisteapp/MainActivityTest.java
@@ -0,0 +1,36 @@
+package com.example.einkaufslisteapp;
+
+import android.content.Context;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import com.example.einkaufslisteapp.Activities.MainActivity;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.einkaufslisteapp", appContext.getPackageName());
+        onView(withId(R.id.buttonListeVerwalten)).perform(click());
+    }
+}
